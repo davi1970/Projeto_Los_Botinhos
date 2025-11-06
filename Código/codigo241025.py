fe9c281bba62d5a30a3057cc42b4566b2606519a
Arclys <arthur.carvalhocorreacerqueira@gmail.com>
2025-11-05 22:38:37 -0400

    reformulação com matriz

--- Código/codigo241025.py
+++ Código/codigo241025.py
@@ -1,64 +1,76 @@
-
 import os 
 import random as rd
 
-sprite = dict[tuple[int],list[str]]
-TELA: sprite = {}
-FRAME: sprite = {}
-TELA_X: int = 20
-TELA_Y: int = 10
-PIXEL_CHEIO: str = "██"
-PIXEL_VAZIO: str = "░░"
-CORES_ANSI = {
-    "PRETO": 30,
-    "VERMELHO": 31,
-    "VERDE": 32,
-    "AMARELO": 33,
-    "AZUL": 34,
-    "MAGENTA": 35,
-    "CIANO": 36,
-    "BRANCO": 37
-    }
+
+
+
+# -- VARIÁVEIS DO JOGO -- #
+amazonino_posicao = 0
+tucuxi_posicao = 0
+
+# - CONSTANTES - #
+
+AZ_C = 51   
+AZUL = 19
+PRET = 0       
+BRAN = 15
+VERD = 46
+VD_E = 22
+ROSA = 200
+CINZ = 245
+
+PIXEL = "██"
+TELA = [
+    [VERD, VERD, VERD, VERD, VERD, VERD, VERD, VERD, VERD, VERD, BRAN, VERD],
+    [AZUL, AZUL, AZUL, VERD, VERD, VERD, VERD, VERD, AZUL, AZUL, PRET, AZUL],
+    [AZ_C, AZ_C, AZUL, AZUL, AZUL, AZUL, AZUL, AZUL, AZUL, AZ_C, BRAN, AZ_C],
+    [AZ_C, AZ_C, AZ_C, AZ_C, AZ_C, AZ_C, AZ_C, AZ_C, AZ_C, AZ_C, PRET, AZ_C],
+    [AZ_C, AZ_C, AZ_C, AZ_C, AZ_C, AZ_C, AZ_C, AZ_C, AZ_C, AZ_C, BRAN, AZ_C],
+    [AZ_C, AZ_C, AZ_C, AZ_C, AZ_C, AZ_C, AZ_C, AZ_C, AZ_C, AZ_C, PRET, AZ_C],
+    [AZUL, AZUL, AZUL, AZUL, AZUL, AZ_C, AZ_C, AZUL, AZUL, AZUL, BRAN, AZUL],
+    [VERD, VERD, VERD, VERD, AZUL, AZUL, AZUL, AZUL, VERD, VERD, PRET, VERD],
+    [VERD, VERD, VERD, VERD, VERD, VERD, VERD, VERD, VERD, VERD, BRAN, VERD]
+]
+
+# -- FUNÇÕES -- #
+
+
+def desenharFrame(frame_atual):
+    for linha in frame_atual:
+        for cor_pixel in linha:
+            printFormatado(cor_pixel, cor_pixel)
+        print()
+
+def rolarDados():
+    amazonino_dado = rd.randint(1,6)
+    tucuxi_dado = rd.randint(1,6)
+    if amazonino_dado > tucuxi_dado:
+        pass
+    elif amazonino_dado < tucuxi_dado:
+        pass
+    else:
+        pass
+    
 
 
 def limparTela():
     os.system("cls" if os.name == "nt" else "clear") # retorna "cls" se o os.name for igual a "nt" (Sistema Operacional do Windows) e caso não seja, retorna "clear" (Usado para Linux e Mac)
-    global FRAME; FRAME = TELA.copy()
-def criarTela(x: int ,y: int) -> None:
-    for _y in range(y):
-        for _x in range(x):
-            TELA[(_x, _y)] = [PIXEL_CHEIO,"AZUL"]
-def desenharFrame(x: int, y: int,SPRITE: sprite) -> None:
-    for _y in range(y):
-        for _x in range(x):
-            caractere, cor = SPRITE[(_x,_y)]
-            printColorido(caractere,cor)
-        print()
-def criarQuadradoCheio(x1:int, y1:int, x2:int, y2:int, cor:str="BRANCO", caractere:str=PIXEL_CHEIO) -> sprite:
-    largura: int = x2 - x1
-    altura: int = y2 - y1
-    imagem_final: sprite = {}
-    for _y in range(altura):
-        for _x in range(largura):
-            imagem_final[(_x, _y)] = [caractere,cor]
-    return imagem_final
-def criarQuadrado(x1:int, y1:int, x2:int, y2:int, cor_cheia:str="BRANCO", caractere_cheio:str=PIXEL_CHEIO,caractere_vazio:str=PIXEL_CHEIO,cor_vazia:str="PRETO") -> sprite:
-    imagem_final = criarQuadradoCheio(x1,y1,x2,y2,cor_cheia,caractere_cheio).copy()
-    imagem_final = criarQuadradoCheio(x1+1,y1+1,x2-1,y2-1,cor_vazia,caractere_vazio)
-    return imagem_final
-def printColorido(texto: str, cor: str) -> None:
-    print(f"\033[{CORES_ANSI[cor]}m{texto}\033[0m", end="")
+    # global FRAME; FRAME = TELA.copy()
+
+
+def printFormatado(cor_texto, cor_fundo = 0, caractere=PIXEL):
+    print(f"\033[38;5;{cor_texto};48;5;{cor_fundo}m{caractere}\033[0m", end="")
 
 
 
 # --EXECUÇÃO DO CÓDIGO-- #
 
-# -Pré-Loop- #
-criarTela(TELA_X,TELA_Y)
-FRAME = TELA.copy()
+# - Pré-Loop - #
+limparTela()
 
+# - Loop - #
 while True:
+    desenharFrame(TELA)
+    input()
     limparTela()
-    desenharFrame(10,10,criarQuadradoCheio(0,0,10,10))
-    
-    # desenharFrame(TELA_X,TELA_Y)
+    #  desenharFrame(TELA_X,TELA_Y)
